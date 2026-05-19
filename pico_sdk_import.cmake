e863c44387488e550e0c863347140296e5a18322
wwtd <51043256+wwtd@users.noreply.github.com>
2026-05-19 21:56:42 +0800

    update config

--- pico_sdk_import.cmake
+++ pico_sdk_import.cmake
@@ -1,9 +1,21 @@
-# This file is copied from the Pico SDK, DO NOT EDIT.
+# Uses the official Pico SDK import mechanism.
+# Set PICO_SDK_PATH as cmake -D or environment variable.
 
-if (DEFINED ENV{PICO_SDK_PATH})
+if (NOT PICO_SDK_PATH)
     set(PICO_SDK_PATH $ENV{PICO_SDK_PATH})
-else()
-    set(PICO_SDK_PATH "/home/pi/pico-dir/pico-sdk")
+endif()
+
+if (NOT PICO_SDK_PATH)
+    foreach(POSSIBLE_SUFFIX pico-sdk pico-sdk-master)
+        if (EXISTS "${CMAKE_CURRENT_LIST_DIR}/${POSSIBLE_SUFFIX}/external/pico_sdk_import.cmake")
+            set(PICO_SDK_PATH "${CMAKE_CURRENT_LIST_DIR}/${POSSIBLE_SUFFIX}")
+            break()
+        endif()
+    endforeach()
+endif()
+
+if (NOT PICO_SDK_PATH)
+    message(FATAL_ERROR "Set PICO_SDK_PATH or place pico-sdk as a sibling directory")
 endif()
 
 include(${PICO_SDK_PATH}/external/pico_sdk_import.cmake)
